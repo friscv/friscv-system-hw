dcfac9c790f2bb1ab58de74d662ecde59f1b0567
Emil Popović <mail@emilpopovic.me>
2026-01-16 11:51:44 +0100

    design 2 integtrated

---
+++ rtl/friscv_core_wrapper.v
@@ -0,0 +1,37 @@
+module friscv_core_wrapper (
+    input wire        i_clk,
+    input wire        i_rstn,
+    
+    // Instruction Memory Interface
+    output wire [31:0] i_mem_addr_out,
+    input  wire [31:0] i_mem_data_in,
+    output wire        i_mem_en_out,
+    input  wire        i_mem_wait_in,
+
+    // Data memory interface 
+    output wire [31:0] d_mem_addr_out,
+    output wire [31:0] d_mem_data_out,
+    input  wire [31:0] d_mem_data_in,
+    output wire        d_mem_en_out,
+    output wire        d_mem_wr_out,
+    output wire [2:0]  d_mem_size_out,
+    input  wire        d_mem_wait_in
+);
+
+friscv_core core_inst (
+    .i_clk          ( i_clk          ),
+    .i_rstn         ( i_rstn         ),
+    .i_mem_addr_out ( i_mem_addr_out ),
+    .i_mem_data_in  ( i_mem_data_in  ),
+    .i_mem_en_out   ( i_mem_en_out   ),
+    .i_mem_wait_in  ( i_mem_wait_in  ),
+    .d_mem_addr_out ( d_mem_addr_out ),
+    .d_mem_data_out ( d_mem_data_out ),
+    .d_mem_data_in  ( d_mem_data_in  ),
+    .d_mem_en_out   ( d_mem_en_out   ),
+    .d_mem_wr_out   ( d_mem_wr_out   ),
+    .d_mem_size_out ( d_mem_size_out ),
+    .d_mem_wait_in  ( d_mem_wait_in  )
+);
+
+endmodule
