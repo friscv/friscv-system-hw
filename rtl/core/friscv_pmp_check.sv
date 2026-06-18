5ea50bd35b72cadbe9ce5e958ef08882ebfedc03
Emil Popović <mail@emilpopovic.me>
2026-06-19 01:51:14 +0200

    pmp tor logic

--- rtl/core/friscv_pmp_check.sv
+++ rtl/core/friscv_pmp_check.sv
@@ -29,4 +29,62 @@ module friscv_pmp_check (
     output logic       o_fault
 );
 
+// The two lowest bits are not used for PMP matching
+addr_t w_aligned_pa;
+assign w_aligned_pa = addr_t'(i_pa[ADDR_WIDTH-1:2]);
+
+function automatic logic fault_for_cfg(pmp_cfg_t cfg);
+    if (cfg.l || (i_mode != M_MODE))
+        // If L set (even in M-mode), or not in M-mode, enforce access type
+        fault_for_cfg = (i_access_r && !cfg.r) ||
+                        (i_access_w && !cfg.w) ||
+                        (i_access_x && !cfg.x);
+    else
+        // L is not set and in M-mode, allow (no fault)
+        fault_for_cfg = 1'b0;
+endfunction
+
+always_comb begin
+    if (i_access_r || i_access_w || i_access_x) begin
+        // By default, succeed in M-mode, fail in S-mode and U-mode
+        o_fault = (i_mode != M_MODE);
+
+        // Start from index 0 of PMP table and return the first match
+        for (int i = 0; i < PMP_ENTRIES-1; i++) begin
+            automatic pmp_entry_t entry = i_pmp_table[i];
+
+            // Match the PMP mode of the i-th entry
+            case (entry.cfg.a)
+
+                // Top of range
+                PMP_TOR: begin
+                    // 0 is the lower bound if 0-th PMP entry is TOR
+                    automatic addr_t prev_addr = (i > 0) ? i_pmp_table[i-1].addr : '0;
+                    // TOR matches, pmpaddr[i-1] <= pa < pmpaddr
+                    if (prev_addr <= w_aligned_pa && w_aligned_pa < entry.addr) begin
+                        o_fault = fault_for_cfg(entry.cfg);
+                        break;
+                    end
+                end
+
+                // Naturally aligned four-byte region
+                PMP_NA4: begin
+                    
+                end
+
+                // Naturally aligned power-of-two region, >= 8 bytes
+                PMP_NAPOT: begin
+                    
+                end
+
+                // Null region (disabled)
+                default: ;
+            endcase
+        end
+    end else begin
+        // No fault if no request
+        o_fault = 1'b0;
+    end
+end
+
 endmodule
